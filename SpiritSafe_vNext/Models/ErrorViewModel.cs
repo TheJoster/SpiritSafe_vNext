60ef6a73611a399cbd64ff21cf682f71bd2fb334
TheJoster <github.com@toele.nl>
2018-03-28 12:46:22 +0200

    Add project files.

---
+++ SpiritSafe_vNext/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpiritSafe_vNext.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
